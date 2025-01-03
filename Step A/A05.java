ec8cef12be1e555e9e9dd696f2f07ef59d131ce8
adakim3297 <holykim3297@naver.com>
2025-01-04 01:36:24 +0900

    StepA

---
+++ Step A/A05.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class A05 {
+    public static void main(String[] args) {
+        A05 stepA = new A05();
+        stepA.a05();
+    }
+
+    void a05(){
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter number of days > ");
+        int days = s.nextInt();
+
+        int seconds = days * 24* 60 * 60;
+        System.out.printf("Total seconds is %d",seconds);
+    }
+}
